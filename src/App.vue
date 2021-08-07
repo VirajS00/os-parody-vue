ba0b7a84da879a56d85f66befe77b170a8259f36
VirajS00 <virajshukla00@gmail.com>
2021-08-07 13:55:47 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="app">
+		<Desktop />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Desktop from './components/Desktop.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		Desktop
+	}
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	position: absolute;
+	inset: 0 0 0 -1px;
 }
 </style>
---
+++ src/components/Desktop.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="desktop">
+		<div class="trash-icon" @click="isSelected" :class="{ selected: isActive }">
+			<img src="../assets/icons/trash.svg" />
+			<span class="text">Trash</span>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Desktop',
+	data() {
+		return { isActive: false };
+	},
+	methods: {
+		isSelected() {
+			this.isActive = !this.isActive;
+		}
+	}
+};
+</script>
+
+<style scoped>
+.desktop {
+	background-image: url(../assets/wallpaper.png);
+	background-size: cover;
+	position: absolute;
+	inset: 0;
+	color: #fff;
+	font-family: 'Poppins', sans-serif;
+	padding: 2em;
+}
+
+.trash-icon {
+	display: flex;
+	flex-direction: column;
+	height: fit-content;
+	width: fit-content;
+	align-items: center;
+	font-size: 1.2rem;
+}
+
+.trash-icon img {
+	margin-bottom: 0.5em;
+	width: 50px;
+}
+
+.trash-icon span {
+	user-select: none;
+	cursor: default;
+}
+
+.selected {
+	position: relative;
+}
+
+.selected::after {
+	content: '';
+	position: absolute;
+	left: -1.1em;
+	top: -0.4em;
+	background-color: #07c3fc86;
+	width: 180%;
+	height: 110%;
+	border-radius: 0.6em;
+}
+</style>
